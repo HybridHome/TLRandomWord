5acdc7a685cbf9eebd71057ad6f9a232b25722f6
lichuanjun <lichuanjun@niwodai.net>
2017-11-13 13:49:35 +0800

    使用 Vue 组件

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,71 +1,18 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        this.setupVue();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         console.log('Received Event: ' + id);
-    },
-    setupVue: function() {
-        var vm = new Vue({
-            el: "#vue-instance",
-            data: {
-                randomWord: '',
-                words: [
-                    'formidable',
-                    'gracious',
-                    'daft',
-                    'mundane',
-                    'onomatopoeia'
-                ]
-            },
-            methods: {
-                getRandomWord: function() {
-                    this.randomWord = '...';
-                    this.$http.get(
-                        'http://api.wordnik.com:80/v4/words.json/randomWord?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
-                    ).then(function (response) {
-                        this.randomWord = response.data.word;
-                    }, function (error) {
-                        alert(error.data);
-                    });
-                }
-            }
-        });
     }
 };
 
